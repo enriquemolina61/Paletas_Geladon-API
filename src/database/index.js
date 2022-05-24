d9e7b906dc5fc5cfdbf96195d91e6992bde050dd
Enrique Molina <76474854+enriquemolina61@users.noreply.github.com>
2022-05-23 22:20:29 -0300

    database

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,7 @@ const paletas = [
     id: 1,
     sabor: 'Açaí com Leite Condensado',
     descricao:
-      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+      'Paleta de Açaí com delicioso recheio de leite condensado!',
     foto: 'assets/images/acai-com-leite-condensado.png',
     preco: 10.0,
   },
@@ -11,7 +11,7 @@ const paletas = [
     id: 2,
     sabor: 'Banana com Nutella',
     descricao:
-      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+      'Paleta sabor banana, com um belo recheio cremoso de Nutella',
     foto: 'assets/images/banana-com-nutella.png',
     preco: 10.0,
   },
@@ -19,9 +19,33 @@ const paletas = [
     id: 3,
     sabor: 'Chocolate Belga',
     descricao:
-      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+      'Paleta sabor chocolate belga, com um sabor incomparável!',
     foto: 'assets/images/chocolate-belga.png',
     preco: 7.0,
+  },
+  {
+    id: 4,
+    sabor: 'Milho Verde',
+    descricao:
+      'Paleta sabor milho verde, o verdadeiro sabor da roça!',
+    foto: 'assets/images/milho-verde.png',
+    preco: 5.0,
+  },
+  {
+    id: 5,
+    sabor: 'Doce de Leite com Recheio de Doce de Leite',
+    descricao:
+      'Paleta sabor doce de leite,com um delicioso recheio cremoso de doce de leite de verdade!',
+    foto: 'assets/images/doce-de-leite-com-doce-de-leite.png',
+    preco: 6.0,
+  },
+  {
+    id: 6,
+    sabor: 'Milho Verde',
+    descricao:
+      'Paleta sabor limão, para dar aquela refrescância!',
+    foto: 'assets/images/limao.png',
+    preco: 4.0,
   },
 ];
 
