0268cb5fbbcd1877ab3746664ec86dd7b6199a56
Enrique Molina <76474854+enriquemolina61@users.noreply.github.com>
2022-05-24 20:27:44 -0300

    update

--- src/database/index.js
+++ src/database/index.js
@@ -2,32 +2,28 @@ const paletas = [
   {
     id: 1,
     sabor: 'Açaí com Leite Condensado',
-    descricao:
-      'Paleta de Açaí com delicioso recheio de leite condensado!',
+    descricao: 'Paleta de Açaí com delicioso recheio de leite condensado!',
     foto: 'assets/images/acai-com-leite-condensado.png',
     preco: 10.0,
   },
   {
     id: 2,
     sabor: 'Banana com Nutella',
-    descricao:
-      'Paleta sabor banana, com um belo recheio cremoso de Nutella',
+    descricao: 'Paleta sabor banana, com um belo recheio cremoso de Nutella',
     foto: 'assets/images/banana-com-nutella.png',
     preco: 10.0,
   },
   {
     id: 3,
     sabor: 'Chocolate Belga',
-    descricao:
-      'Paleta sabor chocolate belga, com um sabor incomparável!',
+    descricao: 'Paleta sabor chocolate belga, com um sabor incomparável!',
     foto: 'assets/images/chocolate-belga.png',
     preco: 7.0,
   },
   {
     id: 4,
     sabor: 'Milho Verde',
-    descricao:
-      'Paleta sabor milho verde, o verdadeiro sabor da roça!',
+    descricao: 'Paleta sabor milho verde, o verdadeiro sabor da roça!',
     foto: 'assets/images/milho-verde.png',
     preco: 5.0,
   },
@@ -42,8 +38,7 @@ const paletas = [
   {
     id: 6,
     sabor: 'Limão',
-    descricao:
-      'Paleta sabor limão, para dar aquela refrescância!',
+    descricao: 'Paleta sabor limão, para dar aquela refrescância!',
     foto: 'assets/images/limao.png',
     preco: 4.0,
   },
