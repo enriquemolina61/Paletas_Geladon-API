04514e3a5265271ab59eefdb2bc2d74fafbdfd04
Enrique Molina <76474854+enriquemolina61@users.noreply.github.com>
2022-06-14 20:18:46 -0300

    fix

--- src/database/index.js
+++ src/database/index.js
@@ -3,10 +3,13 @@ import mongoose from 'mongoose';
 const { connect } = mongoose;
 
 export const conectarAoDatabase = () => {
-  connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  connect(
+    'mongodb+srv://enriquemolina:<845521>@cluster0.y9sez.mongodb.net/elgeladon_db?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  )
     .then(() => {
       console.log('MongoDB Conectado');
     })
